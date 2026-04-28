9b3af312e303cda51fbe409c6c20ebf43892a534
algorist <heathkimdev@gmail.com>
2026-04-29 00:57:58 +0900

    Update Kotlin and IntelliJ platform dependencies to latest versions (#6)
    * build(deps): bump org.jetbrains.kotlin.jvm from 2.3.20 to 2.3.21
    
    Bumps [org.jetbrains.kotlin.jvm](https://github.com/JetBrains/kotlin) from 2.3.20 to 2.3.21.
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.3.20...v2.3.21)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlin.jvm
      dependency-version: 2.3.21
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * build(deps): bump org.jetbrains.intellij.platform from 2.14.0 to 2.15.0 (#5)
    
    Bumps org.jetbrains.intellij.platform from 2.14.0 to 2.15.0.
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.intellij.platform
      dependency-version: 2.15.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.3.20"
-    id("org.jetbrains.intellij.platform") version "2.14.0"
+    id("org.jetbrains.kotlin.jvm") version "2.3.21"
+    id("org.jetbrains.intellij.platform") version "2.15.0"
     id("org.jetbrains.changelog") version "2.5.0"
 }
 
