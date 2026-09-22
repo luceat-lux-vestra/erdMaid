86ae1a245f4217be565baa9ae2053058c8b4c034
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-22 12:46:54 +0900

    hardening(deps): probe IJPGP 2.19 security graph

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.4.20"
-    id("org.jetbrains.intellij.platform") version "2.18.1"
+    id("org.jetbrains.intellij.platform") version "2.19.0"
     id("org.jetbrains.changelog") version "2.5.0"
 }
 
